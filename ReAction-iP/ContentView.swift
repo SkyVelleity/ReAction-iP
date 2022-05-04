7f107639def45a51e58565d894fd41ab0bee2937
Sky Velleity <velleity@outlook.com.au>
2022-05-04 17:30:11 +1000

    "If it builds, it's good" Well I'm glad someone thinks so, because as far as I'm concerned this is a crime against humanity.

--- ReAction-iP/ContentView.swift
+++ ReAction-iP/ContentView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import CoreMotion
+
 
 struct ContentView: View {
     var tapped = false
+
     @State var currentErrorState = "nope"
     @State var x = "No updates yet"
     @State var y = "No updates yet"
     @State var z = "No updates yet"
 
-    var customMotion = CustomMotionManager()
+    let motion = CMMotionManager()
+    
 
+    
+    
     var body: some View {
         if tapped {
             Text("TAPPED")
@@ -31,8 +37,40 @@ struct ContentView: View {
         Text("X: \(x)")
         Text("Y: \(y)")
         Text("Z: \(z)")
-        Button("FUCK") {customMotion.startDeviceMotion(&currentErrorState)}
+       // Button("FUCK") {customMotion.startDeviceMotion(&currentErrorState)}  //Bundle closure that can update XY&Z as well as tapped
     }
+    
+    func startDeviceMotion(_ error: inout String) {
+        if motion.isDeviceMotionAvailable {
+            self.motion.deviceMotionUpdateInterval = 1.0 / 60.0
+            self.motion.showsDeviceMovementDisplay = true
+            self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+            
+            var timer : Timer?
+            timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true, block: { (timer) in
+                if let data = self.motion.deviceMotion {
+                    let x = data.userAcceleration.x
+                    let y = data.userAcceleration.y
+                    let z = data.userAcceleration.z
+                    
+                    print("\(x), \(y), \(z)")
+                }
+                
+            })
+            
+            RunLoop.current.self.add(timer!, forMode: RunLoop.Mode.default)
+            
+        } else {
+            error = "Whoopsies!\nLooks like we did a fucko boingo!"
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -40,3 +78,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
--- ReAction-iP/CustomMotionManager.swift
+++ ReAction-iP/CustomMotionManager.swift
@@ -10,30 +10,8 @@ import CoreMotion
 
 
 class CustomMotionManager {
-    let motion = CMMotionManager()
+    
     var timer : Timer?
     
-    func startDeviceMotion(_ error: inout String) {
-        if motion.isDeviceMotionAvailable {
-            self.motion.deviceMotionUpdateInterval = 1.0 / 60.0
-            self.motion.showsDeviceMovementDisplay = true
-            self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
-            
-            self.timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true, block: { (timer) in
-                if let data = self.motion.deviceMotion {
-                    let x = data.userAcceleration.x
-                    let y = data.userAcceleration.y
-                    let z = data.userAcceleration.z
-                    
-                    print("\(x), \(y), \(z)")
-                }
-                
-            })
-            
-            RunLoop.current.self.add(self.timer!, forMode: RunLoop.Mode.default)
-            
-        } else {
-            error = "Whoopsies!\nLooks like we did a fucko boingo!"
-        }
-    }
+
 }
