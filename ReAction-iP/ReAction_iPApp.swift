1b1f0512d385838bac28eda1011536e8a36ce556
Sky Velleity <velleity@outlook.com.au>
2022-05-04 14:35:13 +1000

    Initial Commit

---
+++ ReAction-iP/ReAction_iPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReAction_iPApp.swift
+//  ReAction-iP
+//
+//  Created by Sky on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct ReAction_iPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
